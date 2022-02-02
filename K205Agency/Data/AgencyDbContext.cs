3a3bd5c7e6a3da33e326af5d85eac4fc63261edb
sananabbasov <sanan_abbasov@list.ru>
2022-02-02 17:23:52 +0400

    02.02.2022 17:00

--- K205Agency/Data/AgencyDbContext.cs
+++ K205Agency/Data/AgencyDbContext.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using K205Agency.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace K205Agency.Data
 {
     public class AgencyDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=AgencyDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Masthead> Mastheads { get; set; }
     }
 }

33581514c65256730d16839ad97a48b645e9e677
sananabbasov <sanan_abbasov@list.ru>
2022-02-02 15:58:54 +0400

    Add project files.

---
+++ K205Agency/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace K205Agency.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

e887d89b3e955fdf284162855ec29c312733abf4
sananabbasov <sanan_abbasov@list.ru>
2022-02-09 16:19:28 +0400

    09.02.2022 16.19

--- K205Agency/Areas/admin/Controllers/PortfoliosController.cs
+++ K205Agency/Areas/admin/Controllers/PortfoliosController.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using K205Agency.Areas.admin.ViewModel;
+using K205Agency.Data;
+using K205Agency.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace K205Agency.Areas.admin.Controllers
 {
     [Area("admin")]
     public class PortfoliosController : Controller
     {
+        private readonly AgencyDbContext _context;
+
+        public PortfoliosController(AgencyDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            AdminVM vm = new()
+            {
+                Portfolios = _context.Portfolios.ToList(),
+            };
+            
+            return View(vm);
         }
     }
 }
---
+++ K205Agency/Areas/admin/ViewModel/AdminVM.cs
@@ -0,0 +1,9 @@
+﻿using K205Agency.Models;
+
+namespace K205Agency.Areas.admin.ViewModel
+{
+    public class AdminVM
+    {
+        public List<Portfolio> Portfolios { get; set; }
+    }
+}

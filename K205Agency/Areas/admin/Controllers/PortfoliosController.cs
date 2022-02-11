ab3eb3b8558cde8851775af7ee3edeee712dde3c
struikrovi1 <sabina20028@gmail.com>
2022-02-11 21:25:21 +0400

    K205 with errors

--- K205Agency/Areas/admin/Controllers/PortfoliosController.cs
+++ K205Agency/Areas/admin/Controllers/PortfoliosController.cs
@@ -20,5 +20,66 @@ namespace K205Agency.Areas.admin.Controllers
         {
             return View(await _context.Portfolios.ToListAsync());
         }
+        public async Task<IActionResult> Detail(int id)
+        {
+            var portfolio = (_context.Portfolios.FirstOrDefault(x => x.ID == id));//bir nece melumat, single birdene.bes firt?
+            if (portfolio == null) return NotFound();
+           
+                return View(portfolio);
+
+
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var portfolio = (_context.Portfolios.FirstOrDefault(x => x.ID == id));//bir nece melumat, single birdene.bes firt?
+            if (portfolio == null) return NotFound();
+
+            return View(portfolio);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Portfolio portfolio)
+        {
+            try
+            {
+                _context.Entry(portfolio);
+                await _context.SaveChangesAsync();  
+            }
+            catch (Exception)
+            { 
+                return NotFound();
+            }
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> Create()
+        {
+
+           
+            ViewBag.Category = _context.Categories.ToList();
+
+            return View();
+        }
+
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Portfolio portfolio)
+        {
+
+
+            _context.Portfolios.Add(portfolio);
+            _context.SaveChanges(); 
+            return View(portfolio);
+        }
     }
 }
---
+++ K205Agency/Migrations/20220211105844_add-migration new-project.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace K205Agency.Migrations
+{
+    public partial class addmigrationnewproject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
